b1e9ab0ef7b5f0466c42c147bb9eea429f73c799
suvaranjan <suvaranjan01@gmail.com>
2024-06-02 21:01:54 +0530

    changes in player.jsx

--- client/src/components/players/Players.jsx
+++ client/src/components/players/Players.jsx
@@ -1,24 +1,34 @@
-import { SearchIcon } from "@chakra-ui/icons";
-import {
-  Box,
-  Button,
-  Checkbox,
-  Input,
-  InputGroup,
-  InputLeftElement,
-  Text,
-} from "@chakra-ui/react";
+import { ExternalLinkIcon } from "@chakra-ui/icons";
+import { Avatar, Box, Button, Text } from "@chakra-ui/react";
 import React, { useState } from "react";
 import MyHeading from "../others/MyHeading";
 import MySearch from "../others/MySearch";
+import toast from "react-hot-toast";
+import { getPlayersBySearch } from "../../apis/api";
 
 function Players() {
   const [inputValue, setInputValue] = useState("");
+  const [players, setPlayers] = useState([]);
+  const [searching, setSearching] = useState(false);
+  const token = localStorage.getItem("token");
 
-  const handleSearch = () => {
-    console.log(inputValue);
+  const handleSearch = async () => {
+    if (inputValue == "") return;
+    try {
+      setSearching(true);
+      const res = await getPlayersBySearch(token, inputValue);
+      setPlayers(res.data);
+      console.log(res);
+    } catch (error) {
+      console.log(error);
+      toast.error(error.response.data.msg);
+    } finally {
+      setSearching(false);
+    }
   };
-
+  const profileClick = (id) => {
+    console.log(id);
+  };
   return (
     <Box mb="2rem">
       <MyHeading
@@ -33,13 +43,57 @@ function Players() {
           handleSearch={handleSearch}
           placeholder="Search Player"
         />
-
-        <Checkbox colorScheme="purple" defaultChecked mt="1rem" mb="1rem">
-          Show All Players
-        </Checkbox>
+        <Box mt="2rem">
+          {players.map((user) => {
+            return (
+              <PlayerCard
+                // searchResults={searchResults}
+                name={user.fullName}
+                photo={user.avatar}
+                playerId={user.playerId}
+                profileClick={profileClick}
+                key={user._id}
+              />
+            );
+          })}
+        </Box>
       </Box>
     </Box>
   );
 }
 
 export default Players;
+
+function PlayerCard({ name, photo, playerId, profileClick }) {
+  return (
+    <Box
+      maxW="600px"
+      display="flex"
+      justifyContent="space-between"
+      alignItems="center"
+      borderRadius="md"
+      mb={3}
+      p={3}
+      borderWidth={1}
+      // bg="#EDF2F7"
+    >
+      <Box display="flex" alignItems="center" mr="3rem">
+        <Avatar src={photo} name={name} size="sm" />
+        <Text fontSize="1rem" fontWeight="500" ml={3}>
+          {name}
+        </Text>
+      </Box>
+
+      <Button
+        colorScheme="green"
+        // size="sm"
+        rightIcon={<ExternalLinkIcon />}
+        mr={4}
+        fontWeight="400"
+        onClick={() => profileClick(playerId)}
+      >
+        Profile
+      </Button>
+    </Box>
+  );
+}
